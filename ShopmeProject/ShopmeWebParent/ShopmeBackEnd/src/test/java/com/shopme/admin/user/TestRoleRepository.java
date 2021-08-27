7b846d88c2d52760b4f3b067fa580fa18314e103
baobeta <lequocbao29072001@gmail.com>
2021-08-27 16:22:28 +0700

    gitignore

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/TestRoleRepository.java
@@ -0,0 +1,51 @@
+package com.shopme.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Role;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace=Replace.NONE)
+@Rollback(false)
+public class TestRoleRepository {
+	 
+	@Autowired
+	private RoleRepository repo;
+	
+	@Test
+	public void testCreateFirstRole() {
+		Role roleAdmin = new Role("Admin", "manage everything");
+		Role saveRole = repo.save(roleAdmin);
+		assertThat(saveRole.getId()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void testCreateRestRole() {
+		Role roleAdmin = new Role("Admin", "manage everything");
+		Role roleSalesperson = new Role("Salesperson", "many product price, "+
+							"customer, shipping, order and sales report");
+		Role roleEditor = new Role("Editor", "manage categories, brands" +
+							", products, acticles and menus");
+		Role roleShiper = new Role("Shiper", "view products, view orders"+
+							" and update order status");
+		Role roleAssistant = new Role("Assistant","manage questions and reviews");
+		
+		repo.saveAll(List.of(roleAdmin,roleSalesperson,roleEditor,roleShiper,roleAssistant));
+		
+		
+	}
+
+	
+
+	
+}
