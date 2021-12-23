bcf6cd37963e5bcd7c6d6d792483d900e988e675
baobeta <lequocbao29072001@gmail.com>
2021-12-23 18:25:06 +0700

    using logging and create log file

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/FileUploadUtil.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/FileUploadUtil.java
@@ -7,10 +7,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
 
 public class FileUploadUtil {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadUtil.class);
+
 	public static void saveFile(String uploadDir, String fileName, 
 			MultipartFile multipartFile) throws IOException {
 
@@ -41,12 +46,14 @@ public class FileUploadUtil {
 					try {
 						Files.delete(file);
 					} catch (IOException ex) {
-						System.out.print("Could not delete file: "+ file);
+						LOGGER.error("Could not delete file: "+ file);
+//						System.out.print("Could not delete file: "+ file);
 					}
 				}
 			});
 		} catch (IOException e) {
-			System.out.print("Could not list directory: "+dirPath);
+			LOGGER.error("Could not list directory: "+dirPath);
+//			System.out.print("Could not list directory: "+dirPath);
 		}
 		
 	}
